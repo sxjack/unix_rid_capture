e466a3c5047ff3cfb9d30efe9624c6e4ea089e32
Steve Jack <66682655+sxjack@users.noreply.github.com>
2022-12-27 08:07:52 +0000

    Add files via upload

--- scripts/display_rid.pl
+++ scripts/display_rid.pl
@@ -9,7 +9,7 @@
 
 use strict;
 use IO::Socket;
-use Curses qw(initscr endwin addstr clear refresh);
+use Curses qw(initscr endwin addstr clear refresh curs_set);
 use JSON qw(decode_json);
 
 my $rid_udp = IO::Socket::INET->new(Proto=>"udp",LocalPort=>32001)
@@ -18,15 +18,20 @@ my $rid_udp = IO::Socket::INET->new(Proto=>"udp",LocalPort=>32001)
 my($datagram,$flags);
 my($line,$text);
 my($mac,$operator,$serial,$latitude,$longitude,$alt_msl,$heading,$speed,$unix_time);
-my($loop_time);
-my($next_row,$row,$col) = (1,0);
+my($loop_time,$debug);
+my($row,$col) = (0,0);
+my($next_row,$time_row,$debug_row) = (1,11,12);
 my($a,$op,$t);
 my(%mac2row,%mac2time,%mac2op);
 my($end_program) = (0);
 
 initscr();
 clear();
+$text = sprintf("%-18s %-20s %-12s %-12s %-4s %7s",
+                "MAC","operator","latitude","longitude","alt.","heading");
+addstr(0,0,$text);
 refresh();
+curs_set(0);
 
 $SIG{INT} = \&catch_int;
 
@@ -67,23 +72,35 @@ for (;!$end_program;) {
                 $row = $mac2row{$mac} = $next_row++;
             }
 
-            $op = ($mac2op{$mac}) ? $mac2op{$mac}: ''; 
+            $op = ($mac2op{$mac})   ? $mac2op{$mac}:   ''; 
+            $t  = ($mac2time{$mac}) ? $mac2time{$mac}: 0; 
         
-            if ($latitude) {
+            $text = sprintf("%-18s %-20s ",$mac,$op);
+            addstr($row,0,$text);
 
-                $text = sprintf("%-18s %-20s %-12.6f %-12.6f %4d %3d",
-                                $mac,$op,$latitude,$longitude,$alt_msl,$heading);
+            if ($latitude) { # We have a Location message.
+
+                $text = sprintf("%-12.6f %-12.6f %4d   %3d  %10d ",
+                                $latitude,$longitude,$alt_msl,$heading,$t);
                 # print "$text\n";
-                addstr($row,0,$text);
+                addstr($row,40,$text);
                 refresh();
             }
+        }
 
-        } elsif ($loop_time = $$a{'loop_time_us'}) {
+        if ($loop_time = $$a{'loop_time_us'}) {
 
-            $row = 16;
             $text = sprintf("%7d us",$loop_time);
             # print "$row loop $loop_time us\n";
-            addstr($row,0,$text);
+            addstr($time_row,0,$text);
+            refresh();
+        }
+
+        if ($debug = $$a{'debug'}) {
+
+            $text = sprintf("%-40s",$debug);
+            # print "$row loop $loop_time us\n";
+            addstr($debug_row,0,$text);
             refresh();
         }
     }
