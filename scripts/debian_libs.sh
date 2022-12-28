13835c10cebed663f77ed508b905998f56e6d19d
Steve Jack <66682655+sxjack@users.noreply.github.com>
2022-12-28 13:53:21 +0000

    Script for installing Debian packages.

---
+++ scripts/debian_libs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# To do: find out how I installed perl IO::Socket.
+#
+
+/usr/bin/apt install libncurses-dev libpcap-dev libgcrypt20 bluez
+
+/usr/bin/apt install libcurses-perl libjson-perl
+
+
