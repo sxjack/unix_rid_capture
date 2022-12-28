6d0ae6f0e9f1af22f5caa9ef89b59d3d77da567d
Steve Jack <66682655+sxjack@users.noreply.github.com>
2022-12-28 08:50:24 +0000

    Decoded timestamps

--- scripts/display_rid.pl
+++ scripts/display_rid.pl
@@ -15,21 +15,27 @@ use JSON qw(decode_json);
 my $rid_udp = IO::Socket::INET->new(Proto=>"udp",LocalPort=>32001)
     or die "Failed to create UDP server: $@";
 
+my($end_program,$op_id) = (0,0);
 my($datagram,$flags);
 my($line,$text);
-my($mac,$operator,$serial,$latitude,$longitude,$alt_msl,$heading,$speed,$unix_time);
+my($mac,$operator,$id,$latitude,$longitude,$alt_msl,$heading,$speed);
+my($hsecs,$unix_time,$m,$s);
+my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst);
 my($loop_time,$debug);
 my($row,$col) = (0,0);
 my($next_row,$time_row,$debug_row) = (1,11,12);
+my($loc_col,$ts_col) = (40,80);
 my($a,$op,$t);
-my(%mac2row,%mac2time,%mac2op);
-my($end_program) = (0);
+my(%mac2row,%mac2time,%mac2op,%mac2id);
 
 initscr();
 clear();
-$text = sprintf("%-18s %-20s %-12s %-12s %-4s %7s",
-                "MAC","operator","latitude","longitude","alt.","heading");
+$text = sprintf("%-18s %-20s ","MAC",($op_id) ? "operator": "ID");
 addstr(0,0,$text);
+$text = sprintf("%-12s %-12s %-4s %7s",
+                "latitude","longitude","alt.","heading");
+addstr(0,$loc_col,$text);
+addstr(0,$ts_col,"   timestamps");
 refresh();
 curs_set(0);
 
@@ -52,11 +58,13 @@ for (;!$end_program;) {
 
             $t         = $$a{'unix time'};
             $op        = $$a{'operator'};
+            $id       = $$a{'uav id'};
             $latitude  = $$a{'uav latitude'};
             $longitude = $$a{'uav longitude'};
             $alt_msl   = $$a{'uav altitude'};
             $heading   = $$a{'uav heading'};
             $speed     = $$a{'uav speed'};
+            $hsecs     = $$a{'seconds'};
 
             if ($t) {
                 $mac2time{$mac} = $t;
@@ -66,6 +74,10 @@ for (;!$end_program;) {
                 $mac2op{$mac} = $op;
             }
 
+            if ($id) {
+                $mac2id{$mac} = $id;
+            }
+
             $row = $mac2row{$mac};
         
             if (!$row) {
@@ -73,17 +85,25 @@ for (;!$end_program;) {
             }
 
             $op = ($mac2op{$mac})   ? $mac2op{$mac}:   ''; 
+            $id = ($mac2id{$mac})   ? $mac2id{$mac}:   ''; 
             $t  = ($mac2time{$mac}) ? $mac2time{$mac}: 0; 
         
-            $text = sprintf("%-18s %-20s ",$mac,$op);
+            $text = sprintf("%-18s %-20s ",$mac,($op_id) ? $op: $id);
             addstr($row,0,$text);
 
             if ($latitude) { # We have a Location message.
 
-                $text = sprintf("%-12.6f %-12.6f %4d   %3d  %10d ",
-                                $latitude,$longitude,$alt_msl,$heading,$t);
+                $m = int($hsecs / 60);
+                $s = int($hsecs % 60);
+                ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime($t);
+
+                $text = sprintf("%-12.6f %-12.6f %4d   %3d   ",
+                                $latitude,$longitude,$alt_msl,$heading);
                 # print "$text\n";
-                addstr($row,40,$text);
+                addstr($row,$loc_col,$text);
+                $text = sprintf("%02d:%02d %2d-%02d-%02d %02d:%02d:%02d ",
+                                $m,$s,$year % 100,$mon + 1,$mday,$hour,$min,$sec);
+                addstr($row,$ts_col,$text);
                 refresh();
             }
         }
