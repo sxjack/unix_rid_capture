965b7f544e2a1d2fdce5580fa7e65c8b85b58c84
Steve Jack <66682655+sxjack@users.noreply.github.com>
2022-12-30 07:50:46 +0000

    Add files via upload

--- scripts/debian_libs.sh
+++ scripts/debian_libs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-# To do: find out how I installed perl IO::Socket.
+# I can't remember how I installed perl IO::Socket, probably 'cpan IO::Socket'.
+# I used cpan to install all the perl modules on my mac -
+# cpan Curses IO::Socket JSON
 #
 
 /usr/bin/apt install libncurses-dev libpcap-dev libgcrypt20 bluez
